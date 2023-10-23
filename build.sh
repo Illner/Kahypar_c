cbda8efd33f4f13dce0a481835d4a627f553337b
illner <illner3@gmail.com>
2023-10-23 16:13:23 +0200

    None

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd build
 # Release
 mkdir Release
 cd Release
-cmake -D CMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../..
+cmake -D CMAKE_BUILD_TYPE=Release -D ENABLE_EXPORTS=TRUE -G "Unix Makefiles" ../..
 make
 cp -R ../../config config
 
